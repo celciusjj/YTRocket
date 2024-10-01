452cbed8f3c38c0b815685ee7061c47df1f1c0ad
celciusjj <juanjjo444@gmail.com>
2024-10-01 15:39:44 -0500

    feat: initial and basic project structure

---
+++ src/screens/MainScreen.tsx
@@ -0,0 +1,11 @@
+import { Sidebar } from "../components/Sidebar";
+
+function MainScreen() {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+}
+
+export default MainScreen;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          main: ['Titillium Web', 'sans-serif']
+        },
+      },
+    },
+    plugins: [],
+  }
