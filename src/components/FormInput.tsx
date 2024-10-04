65c6cbb68695100c4d2078d941006054328f8f96
celciusjj <juanjjo444@gmail.com>
2024-10-03 21:24:12 -0500

    feat: truncate and width background for mobile added

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -17,7 +17,7 @@ export const FormInput = ({ type = 'text', title, form, field, mask }: Props) =>
       name={field}
       value={form?.values?.[field]}
       onChange={form.handleChange}
-      className='text-sm bg-[#BFD2FE33] rounded-[4px] border border-accent h-9 text-secondary pl-3 focus:outline-none focus:ring-0 focus:border-primary group-hover:text-[#1f252d]'
+      className='truncate text-sm bg-[#BFD2FE33] rounded-[4px] border border-accent h-9 text-secondary pl-3 focus:outline-none focus:ring-0 focus:border-primary group-hover:text-[#1f252d]'
     />
   );
 
